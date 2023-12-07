125b0c44bed1b0bf74f86fc19cd9db7c3705a0b6
dino.priyano <dino.priyano@gdn-commerce.com>
2023-12-07 18:48:49 +0700

    :hammer: finalize UI on login screen

---
+++ design-system/src/main/java/id/aej/myflix/design_system/domain/model/PartialClickableTextItems.kt
@@ -0,0 +1,5 @@
+package id.aej.myflix.design_system.domain.model
+
+/**
+ * Created by dinopriyano on 30/11/23.
+ */
---
+++ design-system/src/main/java/id/aej/myflix/design_system/presentation/components/Button.kt
@@ -0,0 +1,5 @@
+package id.aej.myflix.design_system.presentation.components
+
+/**
+ * Created by dinopriyano on 30/11/23.
+ */
